62e62ff902cb8199d7c8b85575737b883c545fae
Pabloin <pablo.inchausti.bf@gmail.com>
2019-04-08 17:16:51 -0300

    Persistencia funciona ok! ....

--- src/main/java/ar/com/bbva/taller/app/model/Customer.java
+++ src/main/java/ar/com/bbva/taller/app/model/Customer.java
@@ -22,6 +22,31 @@ public class Customer {
         this.lastName = lastName;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     @Override
     public String toString() {
         return String.format(
